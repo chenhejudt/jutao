5da4fe4fd16b078958374e3c8d86b6e901684ef7
jutao <861961239@qq.com>
2019-04-13 14:39:26 +0800

    111
    aaa

---
+++ pages/index/index.js
@@ -0,0 +1,77 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+
+Page({
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo')
+    },
+    //事件处理函数
+    bindViewTap: function () {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+
+    //登录跳转
+    bindButtonTap: function(){
+        wx.navigateTo({
+          url: '/pages/home/home',
+        })
+    },
+
+    //注册跳转
+    zhuce: function(){
+        wx.navigateTo({
+            url: '/pages/reg/reg'
+        })
+    },
+
+    //忘记密码跳转
+    forget: function(){
+        wx.navigateTo({
+          url: '/pages/forget/forget',
+        })
+    },
+
+    onLoad: function () {
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
+    },
+    getUserInfo: function (e) {
+        console.log(e)
+        app.globalData.userInfo = e.detail.userInfo
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
+    }
+})
